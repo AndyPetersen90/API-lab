c5d0ad6811f701bd8b642ddbf02097a9ea82c617
Andrew Petersen <fespetersen1990@gmail.com>
2022-05-04 17:30:56 -0600

    Finished last part

--- SWAPI/main.js
+++ SWAPI/main.js
@@ -1,19 +1,23 @@
 
 const buttonPress = document.querySelector('#button');
 
+
 function pressed () {
     // console.log("The button has been pressed");
    
-    axios.get('https://swapi.dev/api/planets/?search=Alderaan')
+    axios.get('https://swapi.dev/api/planets/2')
         .then(res => {
             const planet = res.data;
-            console.log(planet);
-            const val = planet.results.residents
-            console.log(val);
-            for(let i = 0 ; i < reside.length; i++){
-                axios.get(reside[i])
+            const val = planet.residents
+            for(let i = 0 ; i < val.length; i++){
+                axios.get(val[i])
                     .then(response => {
                         console.log(response)
+
+                        let person = document.createElement('h2')
+                        person.textContent = response.data.name
+
+                        document.querySelector('#resident-list').appendChild(person)
                     })
                     .catch(error => {
                         console.log(error)
